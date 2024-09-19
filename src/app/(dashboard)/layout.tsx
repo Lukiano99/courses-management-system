3f0f1e0ec91ec27b6e801f6db688d766aa5e982e
Lukiano99 <l.stojadinovic99@gmail.com>
2024-09-19 15:14:13 +0200

    feat: layout finished

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,17 @@
 import Sidebar from "@/app/(dashboard)/_components/sidebar";
+import Navbar from "./_components/navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
+        <Navbar />
+      </div>
+
       <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
         <Sidebar />
       </div>
-      <main className="md:pl-56">{children}</main>
+      <main className="pt-[80px] md:pl-56">{children}</main>
     </div>
   );
 };
---
+++ src/components/navbar-routes.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const NavbarRoutes = () => {
+  return (
+    <div className="ml-auto flex gap-x-2">
+      <UserButton />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
