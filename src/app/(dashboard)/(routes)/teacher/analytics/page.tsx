061d0379225ec6a5f69af3b1897e6de5735a9474
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-09-29 10:08:59 +0200

    feat: analytics chart

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -2,6 +2,7 @@ import { api } from "@/trpc/server";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import DataCard from "./_components/data-card";
+import Chart from "./_components/chart";
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
@@ -15,7 +16,9 @@ const AnalyticsPage = async () => {
   return (
     <div className="p-6">
       <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
-        <DataCard label="Total sales" value={totalSales} />
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+        <Chart />
       </div>
     </div>
   );
