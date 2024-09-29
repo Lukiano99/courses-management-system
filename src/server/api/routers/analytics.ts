20ebb9115dac6857d9ce7b66d2bcf0ad05bbe543
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-09-29 09:18:06 +0200

    feat: analytics WIP

--- src/server/api/routers/analytics.ts
+++ src/server/api/routers/analytics.ts
@@ -1,8 +1,53 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { Course, Purchase } from "@prisma/client";
 import { z } from "zod";
+
+type PurchaseWithCourse = Purchase & {
+  course: Course;
+};
+
+export const groupByCourse = (purchases: PurchaseWithCourse[]) => {
+  const grouped: Record<string, number> = {};
+
+  purchases.forEach((purchase) => {
+    const courseTitle = purchase.course.title;
+    if (!grouped[courseTitle]) {
+      grouped[courseTitle] = 0;
+    }
+    grouped[courseTitle] += purchase.course.price!;
+  });
+
+  return grouped;
+};
 
 export const analyticsRouter = createTRPCRouter({
   get: protectedProcedure.input(z.object({})).query(async ({ ctx, input }) => {
-    return null;
+    const purchases = await ctx.db.purchase.findMany({
+      where: {
+        course: {
+          userId: ctx.user.id,
+        },
+      },
+      include: {
+        course: true,
+      },
+    });
+
+    const groupedEarnings = groupByCourse(purchases);
+    const data = Object.entries(groupedEarnings).map(
+      ([courseTitle, total]) => ({
+        name: courseTitle,
+        total: total,
+      }),
+    );
+
+    const totalRevenue = data.reduce((acc, curr) => acc + curr.total, 0);
+    const totalSales = purchases.length;
+
+    return {
+      data,
+      totalRevenue,
+      totalSales,
+    };
   }),
 });
