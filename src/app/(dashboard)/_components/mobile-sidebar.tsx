001524db529892b8b82067e6dc4ae6839c7df43d
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-10-03 14:09:29 +0200

    feat: closing sheet on mobile

--- src/app/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,14 +1,22 @@
+"use client";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 import Sidebar from "./sidebar";
+import { useState } from "react";
 
 const MobileSidebar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="pr-4 transition-all hover:opacity-75 md:hidden">
         <MenuIcon />
       </SheetTrigger>
-      <SheetContent side={"left"} className="bg-background p-0">
+      <SheetContent
+        side={"left"}
+        className="bg-background p-0"
+        onClick={() => setOpen(false)}
+      >
         <Sidebar />
       </SheetContent>
     </Sheet>
