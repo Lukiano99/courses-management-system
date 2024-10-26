a805ee145d61a346967afb6d792967981bd25780
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-10-26 10:57:32 +0200

    feat: added console.log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  console.log("It works, world!");
   return (
     <ClerkProvider>
       <html
